cc6878339195983180d10a99819b636b2322f115
shekorla laptop <kassidy.mcconkie@gmail.com>
2024-01-19 12:06:00 -0700

    new overworld overhaul
    also has player inventory code

--- GoldTeamProjMain/Assets/Scripts/TriggerBehavior.cs
+++ GoldTeamProjMain/Assets/Scripts/TriggerBehavior.cs
@@ -3,11 +3,20 @@ using UnityEngine.Events;
 
 public class TriggerBehavior : MonoBehaviour
 {
-    public UnityEvent triggerStartEvent, triggerEndEvent;
+    public UnityEvent triggerStartEvent, triggerEndEvent,playerEvent, weaponEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         triggerStartEvent.Invoke();
+        if (other.CompareTag("Player"))
+        {
+            playerEvent.Invoke();
+        }
+
+        if (other.CompareTag("Weapon"))
+        {
+            weaponEvent.Invoke();
+        }
     }
 
     private void OnTriggerExit(Collider other)
---
+++ GoldTeamProjMain/Assets/Scripts/overworld scripts/RoomData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu]
+public class RoomData : ScriptableObject
+{
+    public GameObject nWall, sWall, eWall, wWall;
+    public Material floorTex;
+}
