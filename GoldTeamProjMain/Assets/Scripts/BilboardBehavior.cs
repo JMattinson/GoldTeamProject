ba26fe751de7d3d2deb36975d69fe09838fb21bc
shekorla laptop <kassidy.mcconkie@gmail.com>
2024-01-19 23:40:17 -0700

    working on resource drops
     this one is very broken, I've got the  resource node, and the drop item, but they aren't connected yet. started messing with enemies, but no progress.

--- GoldTeamProjMain/Assets/Scripts/BilboardBehavior.cs
+++ GoldTeamProjMain/Assets/Scripts/BilboardBehavior.cs
@@ -8,7 +8,7 @@ public class BilboardBehavior : MonoBehaviour
 
     private void Start()
     {
-        focusCam = GameObject.Find("MainCamera").GetComponent<Camera>();
+        //focusCam = GameObject.Find("MainCamera").GetComponent<Camera>();
 
     }
 
