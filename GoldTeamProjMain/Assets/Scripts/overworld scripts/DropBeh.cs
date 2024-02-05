4023d91669846fd732bcf47927a9ebd5c93ea676
shekorla laptop <kassidy.mcconkie@gmail.com>
2024-02-05 12:50:17 -0700

    finished resource drops
    create a prefab variant of the prefab base and then configure as needed. the resource node is placed in the room (for example a tree is a resource node and it drops wood when hit.) the drops need to have the code on the child changed to change its value.

--- GoldTeamProjMain/Assets/Scripts/overworld scripts/DropBeh.cs
+++ GoldTeamProjMain/Assets/Scripts/overworld scripts/DropBeh.cs
@@ -4,14 +4,15 @@ using UnityEngine.Events;
 public class DropBeh : MonoBehaviour
 {
     public UnityEvent death;
-    public void OnTriggerEnter(Collider other)
+    public GameObject parent;
+     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))//dont execute for everything
         {
-            Debug.Log(other);
             //id like to add code here later to allow it to float towards the player
             death.Invoke();//use the event to add to the plr inventory
-            Destroy(this);
+            Destroy(parent);
         }
     }
+
 }
--- GoldTeamProjMain/Assets/Scripts/overworld scripts/NodeBeh.cs
+++ GoldTeamProjMain/Assets/Scripts/overworld scripts/NodeBeh.cs
@@ -10,12 +10,12 @@ public class NodeBeh : MonoBehaviour
     public void SpawnDrops(int dropNum)
     {//this code will spawn the specified number of drops slightly above the node, and launch them slightly
         //you can also add multiple types of drops and it will randomly choose one
-        for (int x = 0; x < dropNum;)
-        {
-            drops = Instantiate(dropPrefab[Random.Range(0,dropPrefab.Length)],(transform.position+new Vector3(0,5,0)) ,new Quaternion(0f,(x*30f),0f,0f));
+        for (int i = 0; i < dropNum;)
+        {   //list of possible drops from this pref     choose random from list                     spawn from top of obj          change the angle it shoots out at diff angle each time
+            drops = Instantiate(dropPrefab[Random.Range(0,dropPrefab.Length)],(transform.position+new Vector3(0,5,0)) ,new Quaternion((i*60f),(i*60f),(i*60f),0f));
             bouncy=(Rigidbody)drops.GetComponentInChildren(typeof(Rigidbody));
-            bouncy.velocity = new Vector3(10, 10, 0);
-            x++;
+            bouncy.velocity = new Vector3(10, 10, 0);//launch so it arcs
+            i++;
         }
         Destroy(gameObject);
     }
