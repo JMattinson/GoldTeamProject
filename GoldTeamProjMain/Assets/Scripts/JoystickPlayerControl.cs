34447c68824606eabcb562a6b59ab7fb5a27ad51
Jaden M <Jadenmattinson6@gmail.com>
2024-02-05 18:10:40 -0600

    Player animation / combo system
    started work on in game animations. alongside a system for "incremental Animations" (animations that play one after the other, like attack strings). still working out the kinks, but looking promising. also completed sprites for wood, located in 2DAssets/Icons/sprites.

--- GoldTeamProjMain/Assets/Scripts/JoystickPlayerControl.cs
+++ GoldTeamProjMain/Assets/Scripts/JoystickPlayerControl.cs
@@ -29,6 +29,7 @@ public class JoystickPlayerControl : MonoBehaviour
         {
             isIdleAnimStart = false;
             StopEvent.Invoke();
+            print("Stopped!");
         }
         
         }
