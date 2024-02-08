6ead5d6ca01a95fada22e2165e8fa623a2727a13
shekorla laptop <kassidy.mcconkie@gmail.com>
2024-02-08 14:29:07 -0700

    working on hunter enemy

--- GoldTeamProjMain/Assets/Scripts/overworld scripts/HunterEneBeh.cs
+++ GoldTeamProjMain/Assets/Scripts/overworld scripts/HunterEneBeh.cs
@@ -6,16 +6,12 @@ public class HunterEneBeh : MonoBehaviour
 {
     //this is just a butchered/edited version of jaden's EnemyBipedBehavior code
     
-     [Header("Entity Management")]
     public UnityEvent deathEv, attackEv, dmgEv;
-    public float MaxHp, CurrentHp;
-    public UnityAction<ImageBehavior> UpdateImage;
-
-
-    [Header("Navigation")]
-    public Transform playerPos; 
+    public float MaxHp, CurrentHp, AtkRng, MvSpeed;
+    //public UnityAction<ImageBehavior> UpdateImage;
+    
+    private Transform playerPos;
     private NavMeshAgent agent;
-    public LayerMask Ground, Player;
     
     public void Start()
     {
@@ -23,17 +19,19 @@ public class HunterEneBeh : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         playerPos = GameObject.Find("Player").transform;
     }
-    public void Hunt()
+    public void Hunt()//gets called when player enters the hunt range collison
     {
-        //if you are not within attack range, then move towards player
-        if (playerPos)//if your close enough to attack then stop moving and hit it
+        //if your close enough to attack then stop moving and hit it
+        if ((Vector3.Distance(gameObject.transform.position,playerPos.position))<=AtkRng)
         {
             // use an animator/animation to make the weapon swing and have some down time
             agent.SetDestination(transform.position);
+            agent.speed = 0;
             attackEv.Invoke();
         }
-        else
+        else//if you are not within attack range, then move towards player
         {
+            agent.speed = MvSpeed;
             agent.SetDestination(playerPos.position);
         }
     }
